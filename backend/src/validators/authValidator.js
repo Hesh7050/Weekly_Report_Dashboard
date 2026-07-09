3aa2b4d61b844a2a2182691d185e374a5988b59d
Heshan Minod <heshhmk@gmail.com>
2026-07-09 12:08:04 +0530

    setup middlewares

---
+++ backend/src/validators/authValidator.js
@@ -0,0 +1,41 @@
+const { body } = require("express-validator");
+
+const registerValidator = [
+  body("name")
+    .notEmpty()
+    .withMessage("Name is required")
+    .isLength({ min: 2 })
+    .withMessage("Name must be at least 2 characters"),
+
+  body("email")
+    .notEmpty()
+    .withMessage("Email is required")
+    .isEmail()
+    .withMessage("Please enter a valid email address"),
+
+  body("password")
+    .notEmpty()
+    .withMessage("Password is required")
+    .isLength({ min: 6 })
+    .withMessage("Password must be at least 6 characters"),
+
+  body("role")
+    .optional()
+    .isIn(["member", "manager"])
+    .withMessage("Role must be either member or manager"),
+];
+
+const loginValidator = [
+  body("email")
+    .notEmpty()
+    .withMessage("Email is required")
+    .isEmail()
+    .withMessage("Please enter a valid email address"),
+
+  body("password").notEmpty().withMessage("Password is required"),
+];
+
+module.exports = {
+  registerValidator,
+  loginValidator,
+};
